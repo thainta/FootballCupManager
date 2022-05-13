80cd67ad78441fff24dd3cb1a5c1fac706b55006
thainta <thai16122001@gmail.com>
2022-05-13 14:26:07 +0700

    update

--- server/index.js
+++ server/index.js
@@ -3,27 +3,14 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-<<<<<<< HEAD
 import userRoutes from './routers/User.js';
-import playerRoutes from './routers/Player.js'
-dotenv.config();
-
-const app = express();
-=======
-import postRoutes from './routers/posts.js';
 import stadiumRoutes from './routers/Stadium.js';
+import playerRoutes from './routers/player.js';
 
 dotenv.config();
 
 const app = express();
 
-app.use('/posts', postRoutes);
-app.use('/stadium', stadiumRoutes);
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
-
->>>>>>> 830b95d (create stadium api)
 const CONNECTION_URL = "mongodb+srv://FootballCupManager:FootballCupManager@cluster0.7hvw3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = 5000;
 
@@ -39,3 +26,4 @@ mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: tru
     
 app.use('/user', userRoutes);
 app.use('/player', playerRoutes);
+app.use('/stadium', stadiumRoutes);
