fc04033837d0fd5440e2444bb430fc1578206440
XuanTruong2408 <truong24082001@gmail.com>
2022-06-10 22:31:09 +0700

    get player by club

--- server/routers/Player.js
+++ server/routers/Player.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {getAllPlayers, getPlayerById, createNewPlayer, updatePlayer, deletePlayer} from '../controllers/Player.js';
+import {getAllPlayers, getPlayerById, createNewPlayer, updatePlayer, deletePlayer, getPlayerByClub} from '../controllers/Player.js';
 
 const router = express.Router();
 
 router.get('/', getAllPlayers);
 router.get('/playerId=:playerId', getPlayerById);
+router.get('/clubId=:clubId', getPlayerByClub)
 router.post('/', createNewPlayer);
 router.put('/playerId=:playerId', updatePlayer);
 router.delete('/playerId=:playerId', deletePlayer);
